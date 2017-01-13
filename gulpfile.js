43fae882a0d38547866b2d8708cc72e27afb79c5
Mladen Plavsic <mladen.plavsic@gmail.com>
2017-01-13 21:40:37 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+var rename = require('gulp-rename');
+var cssnano = require('gulp-cssnano');
+var uglify = require('gulp-uglify');
+
+gulp.task('default', [
+  'docs'
+]);
+
+gulp.task('js', function () {
+  return gulp.src('src/js/*.js')
+    .pipe(concat('masongram.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(uglify({
+      mangle: true
+    }))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('less', function () {
+  return gulp.src('./src/less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('dist'))
+    .pipe(cssnano())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('docs', ['js', 'less'], function () {
+  return gulp.src([
+    './dist/*.min.*'
+  ])
+    .pipe(gulp.dest('docs'))
+});
