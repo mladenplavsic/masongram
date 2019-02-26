341677b38d8c35070fc9b2d2dfd37bf6079044f2
Mladen Plavsic <mladen.plavsic@gmail.com>
2019-02-26 23:18:38 +0100

    Added lightbox support (FancyBox used for demo)

--- src/js/resouorce.js
+++ src/js/resouorce.js
@@ -2,54 +2,54 @@
 
 var Resource = (function () {
 
-  var _config;
-  var _url;
+    var _config;
+    var _url;
 
-  function Resource(config) {
-    _config = config;
-    _url = 'https://api.instagram.com/v1/' + _config.endpoint + '/media/recent/';
-  }
+    function Resource(config) {
+        _config = config;
+        _url = 'https://api.instagram.com/v1/' + _config.endpoint + '/media/recent/';
+    }
 
-  Resource.prototype = {
+    Resource.prototype = {
 
-    load: function () {
-      if (!_url) {
-        return;
-      }
+        load: function () {
+            if (!_url) {
+                return;
+            }
 
-      var self = this;
-      var deferred = $.Deferred();
+            var self = this;
+            var deferred = $.Deferred();
 
-      $.ajax({
-        type: 'POST',
-        url: _url,
-        data: {
-          access_token: _config.access_token,
-          count: _config.count,
+            $.ajax({
+                type: 'POST',
+                url: _url,
+                data: {
+                    access_token: _config.access_token,
+                    count: _config.count,
+                },
+                dataType: 'jsonp',
+            }).success(function (response) {
+                $(self).trigger('loaded', [response.data]);
+                deferred.resolve(response.data);
+                if (!_config.loop) {
+                    // Used for testing
+                    _url = response.pagination && response.pagination.next_url ? response.pagination.next_url : false;
+                }
+            }).error(function () {
+                deferred.reject('Error fetching data');
+            });
+
+            return deferred.promise();
         },
-        dataType: 'jsonp',
-      }).success(function (response) {
-        $(self).trigger('loaded', [ response.data ]);
-        deferred.resolve(response.data);
-        if (!_config.loop) {
-          // Used for testing
-          _url = response.pagination && response.pagination.next_url ? response.pagination.next_url : false;
-        }
-      }).error(function () {
-        deferred.reject('Error fetching data');
-      });
 
-      return deferred.promise();
-    },
+        on: function (name, callback) {
+            $(this).on(name, function (event, data) {
+                callback(data);
+            });
+        },
 
-    on: function (name, callback) {
-      $(this).on(name, function (event, data) {
-        callback(data);
-      });
-    },
+    };
 
-  };
-
-  return Resource;
+    return Resource;
 
 })();
